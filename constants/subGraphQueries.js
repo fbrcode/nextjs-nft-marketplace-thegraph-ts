f8532902ee755f0ce74b0c9480207d5490db5b6d
fbrcode <fabio.bressler@gmail.com>
2022-08-03 20:12:34 -0300

    build up list items graphql

---
+++ constants/subGraphQueries.js
@@ -0,0 +1,21 @@
+import { gql } from "@apollo/client";
+
+const GET_ACTIVE_ITEMS = gql`
+  {
+    activeItems(
+      first: 5
+      where: { buyer: "0x0000000000000000000000000000000000000000" }
+    ) {
+      id
+      buyer
+      seller
+      nftAddress
+      tokenId
+      price
+    }
+  }
+`;
+
+module.exports = {
+  GET_ACTIVE_ITEMS,
+};
