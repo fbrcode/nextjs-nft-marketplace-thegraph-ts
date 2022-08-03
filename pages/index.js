5b12d602c9f87a9c549be987cc9ac698614d0aa3
fbrcode <fabio.bressler@gmail.com>
2022-08-03 20:12:59 -0300

    replace moralis by graphql listing

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,13 @@
-import Image from 'next/image';
-import styles from '../styles/Home.module.css';
-import { useMoralisQuery, useMoralis } from 'react-moralis';
-import NFTBox from '../components/NFTBox';
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useMoralis } from "react-moralis";
+import NFTBox from "../components/NFTBox";
+import {
+  loadDeployedNftMarketplaceContract,
+  networkMapping,
+} from "../constants";
+import { useQuery } from "@apollo/client";
+import { GET_ACTIVE_ITEMS } from "../constants/subGraphQueries";
 
 export default function Home() {
   // How do we show recent listed NFTs?
@@ -12,26 +18,42 @@ export default function Home() {
   // Moralis Server: NFT Marketplace
   // https://admin.moralis.io/dapps/details/198884
   // https://docs.moralis.io/moralis-dapp/automatic-transaction-sync/smart-contract-events
-  const { isWeb3Enabled } = useMoralis();
-  const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
-    'ActiveItem', // TableName
-    (query) => query.limit(10).descending('tokenId') // can also use .skip() for pagination
-  );
+  const { chainId: chainIdHex, isWeb3Enabled } = useMoralis();
+  // this is using Moralis Server implementation which we are replacing by TheGraph
+  // const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery(
+  //   "ActiveItem", // TableName
+  //   (query) => query.limit(10).descending("tokenId") // can also use .skip() for pagination
+  // );
 
-  const myKey = 12345;
+  // this is using TheGraph implementation (GraphQL)
+  const chainId = parseInt(chainIdHex);
+  const networkName = networkMapping[chainId];
+  // console.log(`networkName: ${networkName}`);
+  const nftMarketplaceContract =
+    loadDeployedNftMarketplaceContract(networkName);
+  // console.log(nftMarketplaceContract);
+
+  // const marketplaceAddress = nftMarketplaceContract.address;
+  const marketplaceAddress =
+    nftMarketplaceContract?.address === undefined
+      ? "0x"
+      : nftMarketplaceContract.address;
+
+  // const marketplaceAddress = "0xe088bD8Bb7e0aDcEB50D302bDF49763fD008ABBc";
+
+  const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS);
 
   return (
     <div className="container mx-auto">
       <h1 className="py-4 px-4 font-bold text-2xl">Recently Listed</h1>
       <div className="flex flex-wrap">
         {isWeb3Enabled ? (
-          fetchingListedNfts ? (
+          loading || !listedNfts ? (
             <div>Loading...</div>
           ) : (
-            listedNfts.map((nft) => {
-              // console.log(`NFT Attributes:`);
-              // console.log(nft.attributes);
-              const { marketplaceAddress, nftAddress, tokenId, price, seller } = nft.attributes;
+            listedNfts.activeItems.map((nft) => {
+              // console.log(nft);
+              const { nftAddress, tokenId, price, seller } = nft;
               const key = nftAddress.toString() + tokenId.toString();
               // console.log(`Key: ${key}`);
               // marketplaceAddress: "0xe7f1725e7734ce288f8367e1bb143e90bb3f0512"
