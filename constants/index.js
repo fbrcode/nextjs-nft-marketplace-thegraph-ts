f17b6509859dbeec170b21cac09018418ce1d8f7
fbrcode <fabio.bressler@gmail.com>
2022-08-03 20:10:11 -0300

    update constant utils

--- constants/index.js
+++ constants/index.js
@@ -1,27 +1,30 @@
-const localNftMarketplace = require('./localhost/NftMarketplace.json');
-const localBasicNft = require('./localhost/BasicNft.json');
+const localNftMarketplace = require("./localhost/NftMarketplace.json");
+const localBasicNft = require("./localhost/BasicNft.json");
 
-// const rinkebyNftMarketplace = require('./rinkeby/NftMarketplace.json');
+const rinkebyNftMarketplace = require("./rinkeby/NftMarketplace.json");
+const rinkebyBasicNft = require("./rinkeby/BasicNft.json");
 
 const loadDeployedNftMarketplaceContract = (network) => {
-  if (network === 'localhost') {
+  if (network === "localhost") {
     return localNftMarketplace;
-    // } else if (network === 'rinkeby') {
-    //   return rinkebyNftMarketplace;
+  } else if (network === "rinkeby") {
+    return rinkebyNftMarketplace;
   }
   return null;
 };
 
 const loadDeployedBasicNftContract = (network) => {
-  if (network === 'localhost') {
+  if (network === "localhost") {
     return localBasicNft;
+  } else if (network === "rinkeby") {
+    return rinkebyBasicNft;
   }
   return null;
 };
 
 const networkMapping = {
-  31337: 'localhost',
-  4: 'rinkeby',
+  31337: "localhost",
+  4: "rinkeby",
 };
 
 module.exports = {
